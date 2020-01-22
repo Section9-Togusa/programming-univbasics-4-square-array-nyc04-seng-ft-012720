33de5988c662715c6d8c2ac1fd72eeddc34f1842
Daryl Corbin <corbindac@gmail.com>
2020-01-22 16:09:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
-new_array = [1,4,9]
-array.each do |x|
-  new_array.push x*x
-  end
-  new_array
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
 end
+
+arr = [1, 2, 3, 4, 5]
+square_array(arr)
+# //arr.each { |a| print a -= 10, " " }
+# # prints: -9 -8 -7 -6 -5
+#=> [1, 2, 3, 4, 5]
+
+# basket.each do |apple|
